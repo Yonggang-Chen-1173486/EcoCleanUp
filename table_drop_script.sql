228aa68593a03c4dd5c658169461c53b9d81e9ed
Yonggang-Chen-1173486 <Yonggang.Chen@lincolnuni.ac.nz>
2026-02-28 12:54:14 +0700

    Renamed scripts and added table/type drop scripts

---
+++ table_drop_script.sql
@@ -0,0 +1,9 @@
+drop table feedback CASCADE;
+drop table event_registrations CASCADE;
+drop table events CASCADE;
+drop table event_outcomes CASCADE;
+drop table users CASCADE;
+
+drop type user_role CASCADE;
+drop type user_status CASCADE;
+drop type attendance_status CASCADE;
